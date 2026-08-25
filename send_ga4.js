5808974d140f2eda26109ec8be69ca8c5fbee997
aspire0610 <155468268+aspire0610@users.noreply.github.com>
2026-08-25 10:18:06 +0800

    更新 send_ga4.js

--- send_ga4.js
+++ send_ga4.js
@@ -68,7 +68,7 @@ app.get('/', (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>GA4 發送控制台</title>
+        <title>🌐GA4 發送控制台</title>
         <style>
             * { box-sizing: border-box; }
             body { 
